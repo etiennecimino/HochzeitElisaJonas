9de9090d90e657f8ac98493d0bab97584c186b3e
etiennecimino <etiennecimino2@gmail.com>
2025-08-28 01:35:16 +0200

    Add files via upload

---
+++ scripth.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Hier können wir später JavaScript-Funktionen hinzufügen.
+    // Zum Beispiel für sanftes Scrollen, Navigation, etc.
+    console.log("Website geladen und bereit.");
+});
